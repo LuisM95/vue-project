ee1176ebbebfcc2855ac28bcf4462d19b030c43c
Luis Martel <55332744+LuisM95@users.noreply.github.com>
2020-08-30 23:06:18 +0200

    proyecto de pagina web

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# vue-project
-proyecto de vue
+# Equipo Team CogniRewars
+
+Cognirewars es una plataforma web de gamificación para incentivar la entrega de actividades en entornos escolares.
+
+Aquí deberás escribir toda la información de tu proyecto.
+
+Antes de comenzar, por favor revisa las [las instrucciones](INSTRUCTIONS.md).
+
+![Hack Hero gif](https://media.giphy.com/media/IbHp2s31XVjCyfGZ5L/giphy.gif)
---
+++ src/router/index.js
@@ -0,0 +1,61 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Login from "../views/Login.vue";
+import About from "../views/About.vue";
+import Registro from "../views/Registro.vue";
+import Actividades from "../views/Actividades.vue";
+import Cursos from "../views/Cursos.vue";
+import LoginLayout from "@/components/layout/LoginLayout.vue";
+import ActividadLayout from "@/components/layout/ActividadLayout.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    redirect: "/login",
+    component: LoginLayout,
+    children: [
+      {
+        path: "/login",
+        name: "Login",
+        component: Login,
+      },
+      {
+        path: "/registro",
+        name: "Registro",
+        component: Registro,
+      },
+    ],
+  },
+  {
+    path: "/",
+    redirect: "/actividad",
+    component: ActividadLayout,
+    children: [
+      {
+        path: "/actividad",
+        name: "Actividades",
+        component: Actividades,
+      },
+      {
+        path: "/curso",
+        name: "Cursos",
+        component: Cursos,
+      },
+    ],
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: About,
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
